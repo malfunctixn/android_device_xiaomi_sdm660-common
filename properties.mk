551ee942f99c636acbf1fbe79f539e6f6f473c5f
chrisw444 <wandersonrodriguesf1@gmail.com>
2021-01-24 20:34:00 +0000

    sdm660-common: Import Whyred Parts V2.0
    * XiaomiParts v4 is a combination of several commits and features extracted from ZenParts, XiaomiParts v2 and v3, and DeviceSettings in a single commit, made by @ChrisW444. all authors and contributors are in the authorship.
    * All sepolices and commitment are already in this commit.
    * Features:
      Yellow Brightness Control
      Torch Brightness Control
      Haptic Feedback Control
      Display Color Calibration
      Grayscale & KCAL Presets
      Spectrum Profiles
      Dirac/MI Sound Enhancer
      Headphone Gain
      Microphone Gain
      Earpiece Gain
      Speaker Gain
      Audio High Performance mode
      Log Tile
      FPS Info
      XiaomiDoze
      Touch Boost
      CPU Boost
      GPU Boost
      MSM Thermal Control
      LKM Profiles
      TCP Congestion Algorithm
      SELinux Switch
      Backlight Dimmer
    
    Author: chrisw444 <wandersonrodriguesf1@gmail.com>
    Author: TogoFire <italomellopereira@gmail.com>
    Author: Kingsman44 <shivan.0972@gmail.com>
    Author: SonalSingh18 <SonalSingh18@users.noreply.github.com>
    Author: Sagarrokade006 <sagarrokade006@gmail.com>
    Author: Siddharth Bharadwaj <siddharthbharadwaj45@gmail.com>
    Co-authored-by: Saurabh Charde <saurabhchardereal@gmail.com>
    Co-authored-by: smokey18 <kmboplays@gmail.com>
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Signed-off-by: smokey18 <kmboplays@gmail.com>
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Signed-off-by: Genkzsz11 <genkzsz11@gmail.com>
    
    * Update XiaomiParts v4 for v4.5
      Changelog:
    
    Fix and news sepolices
    Fix and update Spectrum
    Add New Profiles TCP Congestion Algorithm
    Add Cleaner Speaker
    Add Smart Charging
    Add Game Turbo Mode
    Add USB Fasrcharger
    Add BassBooster preset in Dirac
    Add Profile Extreme impuls for CPU in Game Turbo Mode
    Add Translations for PT-BR
    Add HAL Camera L1/L3 QS Tile
    Add Xiaomiparts QS Tile
    Add Dirac Sound QS Tile
    values in Spectrum otimized for kernel sdm660 HMP
    Fps Info redenzing and moved for RIGTH
    
    Author: chrisw444 <wandersonrodriguesf1@gmail.com>
    Author: TogoFire <italomellopereira@gmail.com>
    Author: Genkzsz11 <genkzsz11@gmail.com>
    Co-authored-by: Kingsman44 <shivan.0972@gmail.com>
    Co-authored-by: Dicky Herlambang <herlambangdicky5@gmail.com>
    Co-authored-by: Stargazer19 <pviraj19@gmail.com>
    Co-authored-by: Rafiester <rafi.haqqi@gmail.com>
    Co-authored-by: smokey18 <kmboplays@gmail.com>
    Signed-off-by: smokey18 <kmboplays@gmail.com>
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    
    * Update XiaomiParts v4.5 for v4.6
      Changelog:
    
    Fix Spectrum
    Fix Fps Info
    Misc Changes
    
    Signed-off-by: Logan <athuldineshan5@gmail.com>
    Signed-off-by: Stargazer19 <pviraj19@gmail.com>
    Signed-off-by: P.M.SALMAN KHAN <patanmohammadsalmankhan@gmail.com>
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Signed-off-by: malfunctixn <malfunctixn@gmail.com>

--- properties.mk
+++ properties.mk
@@ -198,4 +198,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # WFD
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    persist.debug.wfd.enable=0
+    persist.debug.wfd.enable=1
+
+# Set lmkd options
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.config.low_ram=false \
+    ro.lmk.use_psi=false
